1ce5c5f8343b9211c9354fd948765c6221e01c2a
Abdul Shaikh <abdulshaikhr99@gmail.com>
2025-09-03 21:01:48 -0400

    fetching simple vpc info

--- data_sources/main.tf
+++ data_sources/main.tf
@@ -2,19 +2,35 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Caller identity (account ID, user ID, ARN)
+
 data "aws_caller_identity" "me" {}
 
-# Current AWS region
+
 data "aws_region" "current" {}
 
 
-# Return full caller identity info
+
 output "aws_caller_identity" {
   value = data.aws_caller_identity.me
 }
 
-# Return current AWS region
+
 output "aws_region" {
   value = data.aws_region.current.name
+}
+
+####
+
+# A data source block (lookup an existing VPC by default filter)
+data "aws_vpc" "consoleVPC" {
+  filter {
+  name   = "tag:Env"
+  values = ["Production"]
+}
+
+}
+
+# Output the ID of the looked-up VPC
+output "non_default_vpc_id" {
+  value = data.aws_vpc.consoleVPC.id
 }
