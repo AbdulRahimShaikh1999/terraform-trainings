87b94aa3c8bbeefa87f3305c853cce61f16f2151
Abdul Shaikh <abdulshaikhr99@gmail.com>
2025-09-05 00:07:46 -0400

    input variables section

--- data_sources/main.tf
+++ data_sources/main.tf
@@ -1,6 +1,17 @@
-provider "aws" {
-  region = "us-east-1"
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
 }
+
+
+
+
 
 
 data "aws_caller_identity" "me" {}
